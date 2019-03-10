3d20be8eee9b73f98bcfb87849c3274f929ef4f4
Alfie Darko <me@alfiedarko.co.uk>
2019-03-10 20:24:13 +0000

    added asset files

---
+++ pages/booking.js
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default () => (
+  <div>
+    <p>This is the bookings page</p>
+  </div>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from "next/link";
+import React, { Component } from "react";
+import "../static/semantic/dist/semantic.css";
+import { Container, Header } from "semantic-ui-react";
+
 const Index = () => (
-    <div>
-      <p>Hello Next.js</p>
-    </div>
-  )
-  
-  export default Index
+  <div>
+    <Container>
+      <Header>Hello Next.js</Header>
+    </Container>
+  </div>
+);
+
+export default Index;
